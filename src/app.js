d8852519df0ffea9e128fda878f6b2dc5a5ce25a
cameeoHub <jacobwkendall@outlook.com>
2022-08-08 08:51:46 +0100

    ref

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 const yargs = require("yargs");
 const { sequelize } = require("./db/connection");
-const { createMovie } = require("./movie/functions");
+const { createMovie, readMovies, updateMovie } = require("./movie/functions");
 
 const app = async (yargsObj) => {
+    await sequelize.sync({ alter: true });
     if (yargsObj.create) {
-        await createMovie({title: yargsObj.title, actor: yargsObj.actor})
+        await createMovie({ title: yargsObj.title, actor: yargsObj.actor })
     } else if (yargsObj.read) {
-
+        await readMovies()
     } else if (yargsObj.update) {
+        const filterObj = { title: yargsObj.title };
+        const updateObj = { title: yargsObj.newTitle };
+        await updateMovie(filterObj, updateObj);
     
     } else if (yargsObj.delete) {
 
--- src/movie/functions.js
+++ src/movie/functions.js
@@ -5,8 +5,45 @@ exports.createMovie = async (movieObj) => {
         await sequelize.sync();
     try {
         const newMovie = await Movie.create(movieObj);
-        console.log(newMovie)
+        console.log(newMovie);
     } catch (error) {
-        console.log(error)
+        console.log(error);
+    }
+};
+
+exports.readMovies = async () => {
+        await sequelize.sync();
+    try {
+        const list = await Movie.findAll({Movie});
+        console.log(list);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.updateMovie = async (movieObj) => {
+    try{
+        if (movieObj.title) {
+            await Movie.update({title: movieObj.newTitle}, {where:{title: movieObj.title}});
+        console.log('The title has been updated')
+        } else if (movieObj.actor) {
+            await Movie.update({actor: movieObj.newActor}, {where:{actor: movieObj.actor}});
+            console.log('The actor has been updated')
+        } else {
+            console.log('Please specify what you would like to update')
+        }
+    }
+    catch(error) {
+        console.log(error);
+    }
+};
+
+exports.deleteMovie = async (movieObj) => {
+    try{
+        await Movie.destroy({where:{title: movieObj.title}});
+        console.log('This entry has been successfully deleted.')
+    }
+    catch(error) {
+        console.log(error);
     }
 };
